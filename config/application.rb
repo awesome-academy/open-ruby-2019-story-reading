964a185ca499c95bd389d3d02f6d78768ca04f1e
Tran Nhat Minh <tn.minh00@gmail.com>
2019-11-25 13:48:09 +0700

    Initialize project

---
+++ config/application.rb
@@ -0,0 +1,11 @@
+require_relative "boot"
+
+require "rails/all"
+
+Bundler.require(*Rails.groups)
+
+module OpenRuby2019StoryReading
+  class Application < Rails::Application
+    config.load_defaults 5.2
+  end
+end
